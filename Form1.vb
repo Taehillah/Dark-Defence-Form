d28eaf9d8dd4d85c483772ab40d10c1ecce1e0e8
Taehillah <ishmaelmafole@gmail.com>
2023-12-06 20:41:32 +0200

    Functionality to the minimize button

--- Form1.vb
+++ Form1.vb
@@ -6,4 +6,8 @@
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
         Application.Exit()
     End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
 End Class
