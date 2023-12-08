44b1bcf804e676c0e68b4f85f5da4c9952fa648b
Taehillah <ishmaelmafole@gmail.com>
2023-12-08 22:18:40 +0200

    exit button activated form2

--- Form2.vb
+++ Form2.vb
@@ -4,4 +4,8 @@
         form1.Show()
         Me.Hide()
     End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Application.Exit()
+    End Sub
 End Class
