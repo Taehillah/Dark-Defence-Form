4d2adc14233b147b0e2f56aa403bd6beb12d629f
Taehillah <ishmaelmafole@gmail.com>
2023-12-08 22:09:01 +0200

    backcolor fill and back button added

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -23,8 +23,11 @@ Partial Class Form2
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         components = New ComponentModel.Container()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
         Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(components)
         Guna2BorderlessForm1 = New Guna.UI2.WinForms.Guna2BorderlessForm(components)
+        Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
         SuspendLayout()
         ' 
         ' Guna2BorderlessForm1
@@ -33,11 +36,30 @@ Partial Class Form2
         Guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6R
         Guna2BorderlessForm1.TransparentWhileDrag = True
         ' 
+        ' Guna2Button1
+        ' 
+        Guna2Button1.CustomizableEdges = CustomizableEdges1
+        Guna2Button1.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button1.Font = New Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Guna2Button1.ForeColor = Color.White
+        Guna2Button1.Location = New Point(327, 386)
+        Guna2Button1.Name = "Guna2Button1"
+        Guna2Button1.ShadowDecoration.BorderRadius = 20
+        Guna2Button1.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2Button1.Size = New Size(150, 25)
+        Guna2Button1.TabIndex = 13
+        Guna2Button1.Text = "NEXT"
+        ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(34), CByte(37), CByte(46))
         ClientSize = New Size(800, 450)
+        Controls.Add(Guna2Button1)
         FormBorderStyle = FormBorderStyle.None
         Name = "Form2"
         Text = "Form2"
@@ -46,4 +68,5 @@ Partial Class Form2
 
     Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
     Friend WithEvents Guna2BorderlessForm1 As Guna.UI2.WinForms.Guna2BorderlessForm
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
 End Class
