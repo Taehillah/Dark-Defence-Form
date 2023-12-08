27c4639d32da3ae3a92d218f705e49dde3f10919
Taehillah <ishmaelmafole@gmail.com>
2023-12-08 22:15:07 +0200

    activated form2 back button

--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form2
-
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Dim form1 As New Form1()
+        form1.Show()
+        Me.Hide()
+    End Sub
 End Class
